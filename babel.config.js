bb1220d41c1275366a5b52fa015e08d1e901b59d
Wallace Júnior <wflj1997@gmail.com>
2024-10-27 15:35:09 -0300

    Variáveis de ambiente

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,14 @@ module.exports = function (api) {
 
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          allowUndefined: false,
+        },
+      ],
+    ],
   };
 };
